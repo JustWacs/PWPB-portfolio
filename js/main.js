e439f09e01ff2b7db776f9a0893d43d2aac32bc4
JustWacs <bagasm072@gmail.com>
2023-10-21 18:20:55 +0800

    Make it responsive

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 const navbar = document.getElementById('navbar');
+const menuToggle = document.getElementById('menu-toggle');
+const menuList = document.getElementById('menu-list');
+
+menuToggle.addEventListener('click', () => {
+    menuList.classList.toggle('hidden');
+});
+
+
 
 window.addEventListener('scroll', () => {
     if(window.document.documentElement.scrollTop > 100) {
